7341b47a89642ce6021f6e8b1adcab02993536c4
hitfishking <1063888400@qq.com>
2018-06-26 23:37:12 +0800

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from django.contrib import admin
+# from .models import Post
+from blog.models import Post
+
+admin.site.register(Post)
---
+++ readme.txt
@@ -0,0 +1,1 @@
+djangogirls.org教程：https://tutorial.djangogirls.org/en/django_start_project/
